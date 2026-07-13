7f8a806f576bc5be83f46fb6c5793a831c6b4a9d
Jordan Perez <jperezherrera@csumb.edu>
2026-07-12 22:14:34 -0700

    Added display, latitude, longitude, and counties based on state

--- lab3/js/script.js
+++ lab3/js/script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", loadStates);
 document.querySelector("#zip").addEventListener("change", displayCity);
 document.querySelector("#username").addEventListener("change", checkUsername);
 document.querySelector("#signupForm").addEventListener("submit", validateForm);
+document.querySelector("#state").addEventListener("change", loadCounties);
 
 async function displayCity() {
   try {
@@ -19,11 +20,14 @@ async function displayCity() {
     }
 
     document.querySelector("#city").textContent = data.city;
+    document.querySelector("#latitude").textContent = data.latitude;
+    document.querySelector("#longitude").textContent = data.longitude;
 
   } catch (error) {
     document.querySelector("#city").textContent = "Unable to retrieve city";
     console.error(error);
   }
+  loadCounties();
 }
 
 async function loadStates() {
@@ -108,5 +112,41 @@ async function validateForm(event) {
 
   if (isValid) {
     document.querySelector("#signupForm").submit();
+  }
+}
+
+async function loadCounties() {
+  let countyMenu = document.querySelector("#county");
+
+  
+  let state = document.querySelector("#state").value;
+
+  countyMenu.textContent = "";
+
+  let defaultOption = document.createElement("option");
+  defaultOption.value = "";
+  defaultOption.textContent = "Select One";
+  countyMenu.appendChild(defaultOption);
+
+  try {
+    let url = `https://csumb.space/api/countyListAPI.php?state=${state}`
+    let response = await fetch(url);
+    let data = await response.json();
+
+    for (let item of data) {
+      let option = document.createElement("option");
+      option.value = state;
+      option.textContent = item.county;
+      countyMenu.appendChild(option);
+    }
+  } catch (error) {
+    console.error(error);
+
+    countyMenu.textContent = "";
+
+    let errorOption = document.createElement("option");
+    errorOption.value = "";
+    errorOption.textContent = "Unable to load states";
+    countyMenu.appendChild(errorOption);
   }
 }
